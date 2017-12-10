f245c76c5d89fffa4fe4f76d462ed098798f5e78
István Schoffhauzer <schoffi92@gmail.com>
2017-12-10 01:08:46 +0100

    Only Scale Applied

--- mesh/mirrorX.py
+++ mesh/mirrorX.py
@@ -6,7 +6,7 @@ for obj in selection:
     mc.select(obj)
     mirrorObjs.append(mc.duplicate()[0])
     mc.scale(-1,1,1)
-    mc.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
+    mc.makeIdentity(apply=True, t=0, r=0, s=1, n=0)
     mc.polyNormal(nm=0)
 
 mc.select(mirrorObjs)
