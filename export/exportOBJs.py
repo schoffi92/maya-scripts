f16f22b3156859cfbae6ca45190b58a61cc276d1
István Schoffhauzer <schoffi92@gmail.com>
2018-01-09 01:28:56 +0100

    Changes And Added Rig

--- export/exportOBJs.py
+++ export/exportOBJs.py
@@ -1,7 +1,11 @@
 import maya.cmds as mc
 import os
 
+
+# Options General: groups=1;ptgroups=1;materials=1;smoothing=1;normals=1
 dirInput = ""
+smoothInput = ""
+materialsInput = ""
 projectPath = mc.workspace(q=True,rootDirectory=True)
 exportPath = projectPath + "data/exports/"
 
@@ -9,9 +13,19 @@ if not os.path.exists(exportPath):
         os.makedirs(exportPath)
 
 def exportAllSelectedGEO(self):
+    options = ""
     dirname = mc.textField(dirInput,q=True,text=True)
+    smooth = mc.checkBox(smoothInput,q=True,value=True)
+    materials = mc.checkBox(materialsInput,q=True,value=True)
+
     if dirname == "":
         dirname = "temp"
+
+    if smooth:
+        options += "smoothing=1;"
+
+    if materials:
+        options += "materials=1;"
     
     directory = exportPath + dirname + "/"
     if not os.path.exists(directory):
@@ -21,12 +35,17 @@ def exportAllSelectedGEO(self):
     # file -force -options "groups=1;ptgroups=1;materials=1;smoothing=1;normals=1" -typ "OBJexport" -pr -es "D:/MayaProjects/MayaPro_Training/data/zbrush/book2/bookPage.obj";
     for obj in selection:
         mc.select(obj)
-        mc.file(directory + obj.replace("|", "_") + ".obj", typ="OBJexport",force=True,pr=True,es=True)
+        mc.file(directory + obj.replace("|", "_") + ".obj", typ="OBJexport",force=True,pr=True,es=True,options=options)
     mc.select(selection)
     pass
 
-wnd = mc.window(title="Export OBJ")
+wnd = mc.window(title="Export OBJ", width=300)
 mc.columnLayout(adjustableColumn=True)
+
+mc.text(label="Options")
+smoothInput = mc.checkBox(label="Smooth",align="right")
+materialsInput = mc.checkBox(label="Materials",align="right")
+
 mc.text(label="Export Directory Name")
 dirInput = mc.textField(text="temp")
 
