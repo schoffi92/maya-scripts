816a4b848df82922049136a0102516ade2062c4d
schoffi92 <schoffi92@gmail.com>
2018-02-26 22:29:49 +0100

    Lock Render Cam Attributes

--- camera/copy-persp.py
+++ camera/copy-persp.py
@@ -1,6 +1,15 @@
 import maya.cmds as mc
 
-# This script copies persp camera and creates from it a "render_cam"
+# This script copies persp camera and locks the copy attributes
 mc.select("persp")
 mc.duplicate(rr=True)
-mc.rename("render_cam")
+objName = mc.rename("render_cam")
+mc.setAttr(objName + ".tx", lock=True)
+mc.setAttr(objName + ".ty", lock=True)
+mc.setAttr(objName + ".tz", lock=True)
+mc.setAttr(objName + ".rx", lock=True)
+mc.setAttr(objName + ".ry", lock=True)
+mc.setAttr(objName + ".rz", lock=True)
+mc.setAttr(objName + ".sx", lock=True)
+mc.setAttr(objName + ".sy", lock=True)
+mc.setAttr(objName + ".sz", lock=True)
